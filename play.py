af7331317f8b8f891baa99cde3e2fb06864fe43d
Brian Erickson <berickson@gmail.com>
2022-05-29 18:57:12 -0700

    adjust initial heading, world frame acceleration

--- play.py
+++ play.py
@@ -65,8 +65,13 @@ def go_straight(droid: SpheroEduAPI, goal_cm: float):
     k_p = 3
     k_d = 3
     d = 0
+    timeout_seconds = goal_cm / 20.0 + 2;
     start_loc = droid.get_location()
+    start_time = time.time()
     while d + goal_tolerance < goal_cm:
+        if time.time()-start_time > timeout_seconds:
+            print("go_straight timed out")
+            break
         error_p = goal_cm - d
         error_d = - get_current_speed(droid)
         sp = k_p * error_p + k_d * error_d
@@ -94,6 +99,20 @@ def turn_to_yaw(droid: SpheroEduAPI, goal_yaw: float):
             break
         time.sleep(0.05)
 
+def x_rotation(vector,theta):
+    """Rotates 3-D vector around x-axis"""
+    R = np.array([[1,0,0],[0,np.cos(theta),-np.sin(theta)],[0, np.sin(theta), np.cos(theta)]])
+    return np.dot(R,vector)
+
+def y_rotation(vector,theta):
+    """Rotates 3-D vector around y-axis"""
+    R = np.array([[np.cos(theta),0,np.sin(theta)],[0,1,0],[-np.sin(theta), 0, np.cos(theta)]])
+    return np.dot(R,vector)
+
+def z_rotation(vector,theta):
+    """Rotates 3-D vector around z-axis"""
+    R = np.array([[np.cos(theta), -np.sin(theta),0],[np.sin(theta), np.cos(theta),0],[0,0,1]])
+    return np.dot(R,vector)
 
 class DataLogger:
 
@@ -102,26 +121,42 @@ class DataLogger:
         self.velocities = []
         self.locations = []
         self.gyros = []
+        self.orientations = []
         self.elapsed_times = []
         self.start_time = time.time()
+        self.pause = False
+        self.world_frame_accelerations = []
+        self.world_frame_acceleration = np.array([0, 0, 0])
     
-    def log(self, droid):
+    def log(self, droid: SpheroEduAPI):
+        if self.pause: return
         loc = droid.get_location()
         v = droid.get_velocity()
         a = droid.get_acceleration()
         gyro = droid.get_gyroscope()
+        orientation = droid.get_orientation()
         self.locations.append(loc)
         self.accelerations.append(a)
         self.velocities.append(v)
         self.gyros.append(gyro)
+        self.orientations.append(orientation)
         self.elapsed_times.append(time.time()-self.start_time)
+
+        world_a = y_rotation(x_rotation(np.array([a["x"], a["y"], a["z"]]), -orientation["pitch"]*math.pi/180),-orientation["roll"]*math.pi/180)
+        self.world_frame_acceleration={"x":world_a[0],"y":world_a[1], "z":world_a[2]}
+        self.world_frame_accelerations.append( self.world_frame_acceleration)
     
     def get_dataframe(self):
+        self.pause = True
         df = pd.DataFrame()
         df["t"] = [t for t in self.elapsed_times]
         df["a_x"] = [a["x"] for a in self.accelerations]
         df["a_y"] = [a["y"] for a in self.accelerations]
         df["a_z"] = [a["z"] for a in self.accelerations]
+
+        df["world_frame_a_x"] = [a["x"] for a in self.world_frame_accelerations]
+        df["world_frame_a_y"] = [a["y"] for a in self.world_frame_accelerations]
+        df["world_frame_a_z"] = [a["z"] for a in self.world_frame_accelerations]
 
         df["v_x"] = [v["x"] for v in self.velocities]
         df["v_y"] = [v["y"] for v in self.velocities]
@@ -132,6 +167,11 @@ class DataLogger:
         df["gyro_x"] = [gyro["x"] for gyro in self.gyros]
         df["gyro_y"] = [gyro["y"] for gyro in self.gyros]
         df["gyro_z"] = [gyro["z"] for gyro in self.gyros]
+
+        df["roll"] = [orientation["roll"] for orientation in self.orientations]
+        df["pitch"] = [orientation["pitch"] for orientation in self.orientations]
+        df["yaw"] = [orientation["yaw"] for orientation in self.orientations]
+        self.pause = False
         return df
     
     def save_csv(self):
@@ -140,8 +180,20 @@ class DataLogger:
 
 logger = DataLogger()      
 
-def on_sensor_msg(*argv):
-    droid = argv[0]
+def on_collision(droid: SpheroEduAPI):
+    print('on_collision')
+
+def on_sensor_msg(droid: SpheroEduAPI):
+    acceleration_crash_threshold = 1
+    
+    a = droid.get_acceleration()
+    total_a = math.hypot(a["x"],a["y"])
+    if total_a > acceleration_crash_threshold:
+        location = droid.get_location()
+        x = location["x"]
+        y = location["y"]
+        print(f"crashed, total_a: {total_a} location: {x},{y}")
+
     # loc = droid.get_location()
     # v = droid.get_velocity()
     # a = droid.get_acceleration()
@@ -151,72 +203,8 @@ def on_sensor_msg(*argv):
     # print(f"x:{loc['x']:4} y:{loc['y']:4} vx:{v['x']:6.2f} vy:{v['y']:<6.2f} ax:{a['x']:6.2f} ay:{a['y']:6.2f} az:{a['z']:6.2f} gyro_x:{gyro['x']:6.1f} gyro_y:{gyro['y']:6.1f} gyro_z:{gyro['z']:6.1f}")
 
 
-while True: # make with breakable
-    with  SpheroEduAPI(toy) as droid:
-        battery_voltage  = Power.get_battery_voltage(toy)
-        print(f"battery_voltage: {battery_voltage}")
-        toy.sensor_control.set_interval(50) # set up faster sensor polling
-        time.sleep(1.0) # give robot time to get some messages
-        droid.register_event(EventType.on_sensor_streaming_data, on_sensor_msg)
-        for _ in range(10):
-            go_straight(droid, 30.0)
-            turn_to_yaw(droid, -droid.get_heading()+90)
-
-        logger.save_csv()
-        break
-
-print("done")
-
-
-def ignore():
-    #t.sensor_control.enable('accelerometer')
-    #t.sensor_control.set_count(1)
-    #t.sensor_control.set_interval(20)
-    # t.add_sensor_streaming_data_notify_listener() throws exceptoin
-
-    droid.set_speed(0)
-    time.sleep(0.1)
-    # print(droid.get_location()['x'])
-    droid.set_main_led(Color(r=0, g=0, b=0))
-    droid.set_back_led(255)
-
-    logger = DataLogger()
-    droid.set_main_led(Color(r=0, g=255, b=0))
-
-
-    droid.set_speed(200)
-    print("should be moving now")
-
-    
-    for _ in range(50):
-        loc = droid.get_location()
-        v = droid.get_velocity()
-        a = droid.get_acceleration()
-        gyro = droid.get_gyroscope()
-
-        logger.log(droid)
-        print(f"x:{loc['x']:4} y:{loc['y']:4} vx:{v['x']:6.2f} vy:{v['y']:<6.2f} ax:{a['x']:6.2f} ay:{a['y']:6.2f} az:{a['z']:6.2f} gyro_x:{gyro['x']:6.1f} gyro_y:{gyro['y']:6.1f} gyro_z:{gyro['z']:6.1f}")
-        time.sleep(0.1)
-
-    print("stopping")
-    droid.set_speed(0)
-    while True:
-        loc = droid.get_location()
-        v = droid.get_velocity()
-        a = droid.get_acceleration()
-        gyro = droid.get_gyroscope()
-
-        logger.log(droid)
-        print(f"x:{loc['x']:4} y:{loc['y']:4} vx:{v['x']:6.2f} vy:{v['y']:<6.2f} ax:{a['x']:6.2f} ay:{a['y']:6.2f} az:{a['z']:6.2f} gyro_x:{gyro['x']:6.1f} gyro_y:{gyro['y']:6.1f} gyro_z:{gyro['z']:6.1f}")
-        vx = v['x']
-        vy = v['y']
-        if math.sqrt(vx*vx+vy*vy) < 10:
-            print("stopped")
-            break
-        time.sleep(0.1)
-
-    logger.save_csv()
-
+def show_charts():
+    global logger
     df = logger.get_dataframe()
     
     plt.subplot(2,2,1)
@@ -247,10 +235,53 @@ def ignore():
     plt.plot(df.t, df.gyro_y, label="y")
     plt.plot(df.t, df.gyro_z, label="z")
     plt.xlabel("t")
-    plt.show()
+    plt.show()    
 
 
-    droid.set_main_led(Color(r=0, g=255, b=255))
+while True: # make with breakable
+    with  SpheroEduAPI(toy) as droid:
 
+        # calibrate
+        droid.set_back_led(255)
+        droid.set_main_led(Color(r=0, g=0, b=0))
+        while True:
+            try:
+                x = int(input("Heading adjust, or Press Enter to continue..."))
+            except:
+                break
+            droid.set_heading(droid.get_heading()+x)
+
+        original_yaw = -droid.get_heading()
+            
+            
+
+
+        droid.set_main_led(Color(r=0, g=0, b=0))
+        droid.set_back_led(255)
+        toy.sensor_control.set_interval(50) # set up faster sensor polling
+        droid.register_event(EventType.on_sensor_streaming_data, on_sensor_msg)
+        droid.register_event(EventType.on_collision, on_collision)
+
+        # make two small squares
+        for _ in range(2):
+            turn_to_yaw(droid, original_yaw+0);
+            go_straight(droid, 30);
+            turn_to_yaw(droid, original_yaw+90);
+            go_straight(droid, 30);
+            turn_to_yaw(droid, original_yaw+180);
+            go_straight(droid, 30);
+            turn_to_yaw(droid, original_yaw+270);
+            go_straight(droid, 30);
         
+        # ram into the wall
+        turn_to_yaw(droid, original_yaw+180);
+        go_straight(droid, 100);
 
+        # run away from
+        turn_to_yaw(droid, 0);
+        go_straight(droid, 30);
+
+        logger.save_csv()
+        break
+
+print("done")
